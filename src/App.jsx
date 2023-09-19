b83695684ed3a401886f59080084cb183c6df2da
James Israel <yongeazi143@gmail.com>
2023-09-19 18:23:52 +0100

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="text-red-800 p-10  m-auto w-60 h-60 translate-y-96 text-6xl font-serif">
+      Let Get Started!!!
+    </div>
+  );
+};
+
+export default App;
